914c1c3fe2b28b2eb30dc736208649de2493d859
Boss <bossnathakorn@gmail.com>
2021-04-17 17:59:19 +0700

    little update fsdb

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -11,12 +11,33 @@ pub struct FileSystemDatabase {
     fs_mutex: Mutex<()>,
 }
 
-const FILE_PATH: &str = "todos.json";
+const FILE_PATH: &str = "db";
+
+enum Entity {
+    Todo
+}
+
+impl Entity {
+    fn name(&self) -> &str {
+        match self {
+            Self::Todo => "todo"
+        }
+    }
+}
+
+fn entity_file_path(entity: Entity) -> String {
+    let path = format!("{}/{}.json", FILE_PATH, entity.name());
+    let p = std::path::Path::new(FILE_PATH);
+    if !p.exists() {
+        std::fs::create_dir(p).expect("Cannot create dir that does not exist");
+    }
+    path
+}
 
 impl FileSystemDatabase {
     fn get_todos(&self) -> Vec<Todo> {
         let _guard = self.fs_mutex.lock();
-        let file_content = std::fs::read_to_string(FILE_PATH);
+        let file_content = std::fs::read_to_string(entity_file_path(Entity::Todo));
         if let Ok(todos_json) = file_content {
             let todos = serde_json::from_str::<Vec<Todo>>(&todos_json);
             if let Ok(todos) = todos {
@@ -33,7 +54,7 @@ impl FileSystemDatabase {
         let _guard = self.fs_mutex.lock();
         let todos_json = serde_json::to_string(&todos);
         if let Ok(todos_json) = todos_json {
-            std::fs::write(FILE_PATH, &todos_json).unwrap();
+            std::fs::write(entity_file_path(Entity::Todo), &todos_json).unwrap();
         }
     }
 }
